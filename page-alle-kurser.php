aa9e9e7de428a5521367b12f9f334e6bc04a2d17
Mathilde <mathilde68n8@stud.kea.dk>
2021-11-06 18:18:28 +0100

    allekurserpage

--- page-alle-kurser.php
+++ page-alle-kurser.php
@@ -10,34 +10,33 @@ get_header();
 
 
 
-	<div id="section" class="content-area">
+	<section id="section" class="content-area">
 
 		<main id="main" class="site-main">
-
+			<h1 id="add"></h1>
 		</main><!-- #main -->
-		<script>
- let kurser;
+		
+<script>let kurser;
       
-        //url til restdb 
-        const url = "https://xn--mflingo-q1a.dk/kea/ungdomsbyen/wp-json/wp/v2/kursus";
-       
+	  //url til restdb 
+	  const url = "https://xn--mflingo-q1a.dk/kea/ungdomsbyen/wp-json/wp/v2/kursus";
+	 
 
-        // asynkron function som afventer og indhenter json data fra restdb
-        async function hentData() {
-            const jsonData = await fetch(url);
-            kurser = await jsonData.json();
-            visKurser();
-        }
+	  // asynkron function som afventer og indhenter json data fra restdb
+	  async function hentData() {
+		  const jsonData = await fetch(url);
+		  kurser = await jsonData.json();
+		  visKurser();
+	  }
 
-		function visKurser(){
-			console.log(kurser);
-		}
-
-
+	  function visKurser(){
+		  console.log(kurser);
+		  document.getElementById("add").innerHTML = "changed!";
+	  }
 
 </script>
-	</div><!-- #section -->
 
+	</section><!-- #section -->
 
 
 <?php
